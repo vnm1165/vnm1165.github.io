527528fe5b27ab558ca74abddac0d249635924a5
VNM Evony <117020747+vnm1165@users.noreply.github.com>
2022-10-30 20:22:32 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+var data = [{
+        link: "https://evonyguidewiki.com/en/index-en/",
+        title: "Evony: The King's Return GUIDE WIKI"
+    },
+    {
+        link: "https://evonyguidewiki.com/en/how-many-troops-defeat-boss-en/",
+        title: "Cần bao nhiêu quân để hạ boss"
+    },
+]
