5918314d2ed69ed4b5b81dad03dc64dfb3c8156a
VNM Evony <117020747+vnm1165@users.noreply.github.com>
2022-11-04 13:37:13 +0700

    Update data.js

--- data.js
+++ data.js
@@ -5,7 +5,11 @@ var data = [{
     {
         link: "https://evonyguidewiki.com/en/how-many-troops-defeat-boss-en/",
         title: "Cần bao nhiêu quân để hạ boss"
-    },
+    },    
+    {
+        link: "https://evonyguidewiki.com/en/how-to-add-skill-en/",
+        title: "Cách thêm skill"
+    },        
     {
         link: "https://drive.google.com/drive/folders/1CszogpEoSvWj0OeSnZ8VemYiiaaKetEk?usp=sharing",
         title: "[WAO]ththvinh27"
