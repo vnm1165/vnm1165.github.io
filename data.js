3527fd1158f270ae50f58585394d6cfb7a67107d
VNM Evony <117020747+vnm1165@users.noreply.github.com>
2022-12-08 18:33:54 +0700

    Update data.js

--- data.js
+++ data.js
@@ -15,7 +15,7 @@ var data = [{
         title: "[WAO]ththvinh27"
     },        
     {
-        link: "https://vnm1165.github.io/calc",
+        link: "https://vnm1165.github.io/calc/rss.html",
         title: "Tính nhanh RSS"
     },
 ]
